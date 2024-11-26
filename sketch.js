1da29ff594a286ba27d4bf3e6c8b3ae0de2292b5
SeungBum Kim <picxenk@gmail.com>
2024-11-26 14:32:42 +0900

    Repeller 추가

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
-let system;
 let systems = [];
+let repeller;
 
 let g; // gravity
 let wind;
 
 function setup() {
   createCanvas(720, 400);
-  // system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
   wind = createVector(0.03, -0.01);
+
+  repeller = new Repeller(width / 2 - 20, height / 2);
 }
 
 function draw() {
@@ -18,8 +19,12 @@ function draw() {
     s.addParticle();
     s.applyGravity(g);
     s.applyForce(wind);
+    s.applyRepeller(repeller);
     s.run();
   }
+
+  repeller.show();
+
 }
 
 function mouseClicked() {
