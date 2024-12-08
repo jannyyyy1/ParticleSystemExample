5682f47702902db64ea147776feb07cb1bf220bb
jannyyyy1 <jannyzzz@naver.com>
2024-12-08 22:52:22 +0900

    비행기의 목표 라인 표시됨

--- Aircraft.js
+++ Aircraft.js
@@ -45,6 +45,13 @@ class Aircraft {
         this.position.x - 10, this.position.y + 10,
         this.position.x + 10, this.position.y + 10
       );
+
+          // Target 표시
+    if (this.target) {
+        stroke(200, 50, 50, 150);
+        line(this.position.x, this.position.y, this.target.x, this.target.y);
+      }
     }
   }
+  
   
