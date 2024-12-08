d001862d8689ad16c060b7d35b6ed6721f2b2fbd
jannyyyy1 <jannyzzz@naver.com>
2024-12-08 22:19:43 +0900

    particle system 클래스에서 비행기 영향 메서드 추가

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -18,6 +18,14 @@ class ParticleSystem {
         }
     }
 
+    applyAircraftForce(aircraftPos) {
+        let force = p5.vector.sub(aircraftPos, this.origin);
+        force.setMag(0.05); //힘크기 조절
+        for (let p of this.particles){
+            p.applyForce(force);
+        }
+    }
+
     run() {
         for (let i = this.particles.length-1; i >= 0; i--) {
             let p = this.particles[i];
